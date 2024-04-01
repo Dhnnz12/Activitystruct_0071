af168e5e5135081a0862bbd2e19d88d7560116ab
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-01 14:42:47 +0700

    menampilkan data alamat mahasiswa

--- Activitystruct_071/Struktur 3 B.cpp
+++ Activitystruct_071/Struktur 3 B.cpp
@@ -20,6 +20,10 @@ int main() {
     getline(cin , mhs.nim);
     cout << "Nama Mahasiswa : ";
     getline(cin,mhs.nama);
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
     
 
 }
