0197b7c8fd9883197cf797fdf1b2fe9d664ef4ed
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-01 14:28:23 +0700

    menampilkan data alamat

--- Activitystruct_071/Struktur 2 B.cpp
+++ Activitystruct_071/Struktur 2 B.cpp
@@ -31,6 +31,6 @@ int main(){
 
     cout << endl;
     cout << "\n NIM : "<< mhs.nim;
-    cout << "\n Nama :" << mhs.nama;
-    
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
 }
