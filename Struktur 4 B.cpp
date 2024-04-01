e7fc46334713f088926f5821d1c93c6cad388049
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-01 15:01:06 +0700

    menampilkan alamat desa

--- Struktur 4 B.cpp
+++ Struktur 4 B.cpp
@@ -18,6 +18,12 @@ int main(){
     Mahasiswa mhs[3];
     for (int i=0,i<3;i++){
     cout << "Nomor Mahasiswa : ";
-    getline(cin , mhs.nim);   
+    getline(cin ,mhs.nim); 
+    cout << "Nama Mahasiswa : ";
+    getline(cin ,mhs.nama);   
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
     }
 }
